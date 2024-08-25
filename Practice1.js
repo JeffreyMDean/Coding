7d0732e42fe07a37bf036788596ef1116097ee99
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-08-25 14:40:31 -0400

    practice1.js

---
+++ Practice1.js
@@ -0,0 +1,32 @@
+// Start with an array of strings and combine them all into a single string.
+// For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+var strings = ["volleyball", "basketball", "badminton"];
+var singleString = "";
+var index = 0;
+while (index < strings.length) {
+  singleString = singleString + strings[index];
+  index++;
+}
+console.log(singleString);
+
+var strings = ["volleyball", "basketball", "badminton"];
+var singleString = "";
+strings.forEach(function (string) {
+  singleString = singleString + string;
+});
+console.log(singleString);
+
+var strings = ["volleyball", "basketball", "badminton"];
+var singleString = strings.reduce(function (single, string) {
+  return single = single + string;
+}, "");
+console.log(singleString);
+
+// Implement a function named factorial that has one parameter: an integer, n. It must return the value of  (i.e.,  factorial).
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(4));
