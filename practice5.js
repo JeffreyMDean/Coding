52d6dbd0937c42d10736ca35e8552fada2568354
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-08-31 16:41:20 -0400

    Create practice6.js

--- practice5.js
+++ practice5.js
@@ -12,11 +12,16 @@ var twoSum = function(nums, target) {
     const complement = target - nums[i];
 
     if (numMap.has(complement)) {
-      return [numMap.get(complement), i];
-    }
+      return [numMap.get(complement), i];  
+    }         // calls get method on numMap object to retrieve the value associated with the complement key
+              // returning array of 2 elements:
+              // 1) the index of the number in 'nums' that when added to nums[i], equals the target
+              // 2) current index in the 'nums' array
+              // returns [1, 0]
 
     numMap.set(nums[i], i);
-  }
+  }        // during first iteration, numMap does not have 7, so you store 2 in 'NumMap' with its index: 'NumMap(2, 0)'
+          // second iteration: nums[1] is 7, complement needed is 9 - 7 = 2, function returns [0, 1], where 0 is the index from 'NumMap' and 1 is the current index 'i'
 
   return [];
 };
