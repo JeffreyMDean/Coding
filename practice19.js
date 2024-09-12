5821d681ce268f9cb8b0f1c4c3e177b2a9345daf
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-09-12 09:51:58 -0400

    Added problem to practice19.js

--- practice19.js
+++ practice19.js
@@ -17,3 +17,28 @@ function combinationOfProducts(numbers) {
 
 const numbers = [2, 8, 3];
 console.log(combinationOfProducts(numbers));
+
+// Use a nested loop to find the largest sum of any two different numbers within an array.
+// For example, [1, 8, 3, 10] becomes 18.
+
+function largestSum(nums) {
+  maxSum = nums[0] * nums[1];
+  let index1 = 0;
+  while (index1 < nums.length) {
+    let index2 = 0;
+    while (index2 < nums.length) {
+      if (index1 !== index2) {
+        sum = nums[index1] + nums[index2];
+        if (sum > maxSum) {
+          maxSum = sum;
+        }
+      }
+      index2++;
+    }
+    index1++;
+  }
+  return maxSum;
+}
+
+const nums = [1, 8, 3, 10];
+console.log(largestSum(nums));
