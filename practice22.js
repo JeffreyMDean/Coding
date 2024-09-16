900cca7d8590968c9d8cf9c341724b79e4799a85
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-09-16 16:23:39 -0400

    Added problems to practice22.js

--- practice22.js
+++ practice22.js
@@ -35,3 +35,21 @@ function numberAsInteger(number) {
 const number = 99.99;
 console.log(numberAsInteger(number));
 // after 99.99 is converted to the string "99.99", it stops parsing when it encounters a character that is not a valid part of an integer (in this case the period), so the function returns the integer part of the string before the period which is "99" which is then converted to the number 99
+
+// Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+function numbersMultiplied(num1, num2) {
+  return num1 * num2;
+}
+
+const num1 = 2;
+const num2 = 4;
+console.log(numbersMultiplied(num1, num2));
+
+// Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+function lowercaseLetters(animal) {
+  return animal.toLowerCase();
+}
+const animal = "Penguin";
+console.log(lowercaseLetters(animal));
+            // effectively becomes lowecaseLetters("Penguin" when called)
