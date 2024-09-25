0add95591d295a9de0c5428d30cd8267c6ae8a9d
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-09-25 15:24:43 -0400

    Added problem to practice29.js

--- practice29.js
+++ practice29.js
@@ -59,3 +59,31 @@ function test4(number1) {
 
 const number1 = 84;
 console.log(test4(number1));
+
+// Write a program that stores a customer's age and a movie's time in two separate variables. Then calculate the price of a movie ticket based on the following conditions:
+
+// If the age is 12 years old or younger, the ticket price is $5.
+// If the age is between 13 and 59 years old and the movie is before 6 PM, the ticket price is $7. After 6 PM, the ticket price is $10.
+// If the customer is 60 years old or older, the ticket price is $7.
+
+function movieTheater(age, movieTime) {
+  let ticketPrice;
+
+  if (age <= 12) {
+    ticketPrice = 5;
+  } else if (age >= 13 && age <= 59) {
+    if (movieTime < 18) {
+      ticketPrice = 7;
+    } else {
+      ticketPrice = 10;
+    }
+  } else if (age >= 60) {
+    ticketPrice = 7;
+  }
+  return ticketPrice;
+}
+
+const age = 34;
+const movieTime = 19;
+const ticketPrice = movieTheater(age, movieTime);
+console.log(`The ticket price for a ${age}-year-old at ${movieTime}:00 is $${ticketPrice}`);
