91e80665cd84af92f5ce5c7b7988e31495fa8487
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-09-30 14:35:04 -0400

    Added problem to practice33.js

--- practice33.js
+++ practice33.js
@@ -31,3 +31,35 @@ const membershipLevel = "regular";
 const discount = discountCalculator(orderValue, membershipLevel);
 console.log(`Your discount is $${discount}`);
 
+// Write a Ruby program that stores the weight of a package and the destination (domestic or international). Then calculate the shipping fee based on the following conditions:
+
+// If the destination is domestic:
+// If the weight is less than or equal to 1 kg, the shipping fee is $5.
+// If the weight is greater than 1 kg, the shipping fee is $10.
+// If the destination is an international shipment:
+// If the weight is less than or equal to 1 kg, the shipping fee is $15.
+// If the weight is greater than 1 kg, the shipping fee is $25.
+
+function shippingCalculator(packageWeight, destination) {
+  let shippingFee = 0;
+
+  if (destination === "domestic") {
+    if (packageWeight <= 1) {
+      shippingFee = 5;
+    } else {
+      shippingFee = 10;
+    }
+  } else if (destination === "international") {
+    if (packageWeight <= 1) {
+      shippingFee = 15;
+    } else {
+      shippingFee = 25;
+    }
+  }
+  return shippingFee;
+}
+
+const packageWeight = 2;
+const destination = "international";
+const shippingFee = shippingCalculator(packageWeight, destination);
+console.log(`Your shipping fee is $${shippingFee}`);
