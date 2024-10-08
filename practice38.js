f0c6b989cb44cdea22887b2a983b70726fd26fa2
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-10-08 10:57:45 -0400

    Added problem to practice38.js

--- practice38.js
+++ practice38.js
@@ -10,3 +10,15 @@ function numberPrinter5() {
 }
 
 numberPrinter5();
+
+// Write a while loop that prints the number 9000 ten times.
+
+function numberPrinter() {
+  let count = 0;
+  while (count < 10) {
+    console.log(9000);
+    count++;
+  }
+}
+
+numberPrinter();
