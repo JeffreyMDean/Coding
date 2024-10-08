aeb5d28fe3b986ddc39f9535c6f3f6df1b5fc52e
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-10-08 09:06:00 -0400

    Create practice38.js

---
+++ practice38.js
@@ -0,0 +1,12 @@
+// Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+function numberPrinter5() {
+  let number = 0;
+
+  while (number <= 100) {
+    console.log(number);
+    number += 5;
+  }
+}
+
+numberPrinter5();
