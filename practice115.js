20fc86dad88280bfeb552121e6fd38d4d829b89e
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-12-30 11:49:42 -0500

    Create practice115.js

---
+++ practice115.js
@@ -0,0 +1,53 @@
+// Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+// For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+
+function letterCounter(string) {
+  let hash = {};
+  let i = 0;
+
+  while (i < string.length) {
+    let letter = string[i];
+    if (hash[letter] === undefined) {
+      hash[letter] = 0;
+    }
+    hash[letter]++;
+    i++;
+  }
+
+  return hash;
+}
+
+const inputString = "bookkeeper";
+const result = letterCounter(inputString);
+console.log(result);
+
+function letterCounter2(string) {
+  let hash = {};
+
+  for (let letter of string) {
+    if (hash[letter] === undefined) {
+      hash[letter] = 0;
+    }
+    hash[letter]++;
+  }
+
+  return hash;
+}
+
+const inputString2 = "bookkeeper";
+const result2 = letterCounter2(inputString2);
+console.log(result2);
+
+function letterCounter3(string) {
+  return string.split("").reduce((hash, letter) => {
+    if (hash[letter] === undefined) {
+      hash[letter] = 0;
+    }
+    hash[letter]++;
+    return hash;
+  }, {});
+}
+
+const inputString3 = "bookkeeper";
+const result3 = letterCounter3(inputString3);
+console.log(result3);
