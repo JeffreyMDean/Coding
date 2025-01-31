c0e15861c9c39af70649520d7f44598e339def29
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-01-31 10:41:59 -0500

    Create practice147.js

---
+++ practice147.js
@@ -0,0 +1,60 @@
+// Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
+// For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+
+function converter(items) {
+  return items.reduce((hash, item) => {
+    let key = item.id;
+    let value = item;
+    hash[key] = value;
+    return hash;
+  }, {});
+}
+
+const inputItems = [
+  { id: 1, color: "blue", price: 32 },
+  { id: 2, color: "red", price: 12 },
+];
+const result = converter(inputItems);
+console.log(result);
+
+// Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
+// For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
+
+function converter2(people) {
+  let array = [];
+
+  for (let id in people) {
+    if (people.hasOwnProperty) {
+      array.push({ id: parseInt(id), ...people[id] });
+    }
+  }
+
+  return array;
+}
+
+const inputPeople = {
+  321: { name: "Alice", age: 31 },
+  322: { name: "Maria", age: 27 },
+};
+const result2 = converter2(inputPeople);
+console.log(result2);
+
+// Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
+// For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
+
+function converter3(strings) {
+  let hash = {};
+
+  for (let i = 0; i < strings.length; i++) {
+    if (hash[strings[i]] === undefined) {
+      hash[strings[i]] = 0;
+    }
+    hash[strings[i]]++;
+  }
+
+  return hash;
+}
+
+const inputStrings = ["do", "or", "do", "not"];
+const result3 = converter3(inputStrings);
+console.log(result3);
