5e0a4d4d5a0910853d99f855eebf03ac06ef7928
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-02-20 11:49:15 -0500

    Create practice167.js

---
+++ practice167.js
@@ -0,0 +1,19 @@
+// Write a method that takes in a number and returns the number times two. Then run the method and print the result.
+
+function getDouble(number) {
+  return number * 2;
+}
+
+const inputNumber = 7; // input value
+const result = getDouble(inputNumber);
+console.log(result);
+
+// Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
+
+function capLetters(string) {
+  return string.toUpperCase();
+}
+
+const inputString = "book";
+const result2 = capLetters(inputString);
+console.log(result2);
