971ce613f1fcdb4bf82e6aa6ff6b6431d0273185
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-02-21 09:41:36 -0500

    Create practice168.js

---
+++ practice168.js
@@ -0,0 +1,10 @@
+// Write a method that takes in two numbers and returns the first number subtracted by the second. Then run the method and print the result.
+
+function subtractor(number1, number2) {
+  return number1 - number2;
+}
+
+const inputNumber1 = 2;
+const inputNumber2 = 5;
+const result = subtractor(inputNumber1, inputNumber2);
+console.log(result);
