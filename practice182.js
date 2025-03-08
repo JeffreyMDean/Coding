ff9915f76b8de330fe651e52ef06034c1037299f
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-03-08 05:58:54 -0500

    Create practice182.js

---
+++ practice182.js
@@ -0,0 +1,85 @@
+// Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+// For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+function greaterThan5(items) {
+  return items.filter(function (item) {
+    return item["price"] > 5;
+  });
+}
+
+const inputItems = [
+  { name: "chair", price: 100 },
+  { name: "pencil", price: 1 },
+  { name: "book", price: 4 },
+];
+const result = greaterThan5(inputItems);
+console.log(result);
+
+// Start with an array of numbers and create a new array with only the even numbers.
+// For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+function evenNumbers(numbers) {
+  return numbers.filter((number) => {
+    return number % 2 === 0;
+  });
+}
+
+const inputNumbers = [2, 4, 5, 1, 8, 9, 7];
+const result2 = evenNumbers(inputNumbers);
+console.log(result2);
+
+// Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+// For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+function noB(strings) {
+  return strings.filter((string) => {
+    return string[0] !== "b";
+  });
+}
+
+const inputStrings = ["big", "little", "good", "bad"];
+const result3 = noB(inputStrings);
+console.log(result3);
+
+// Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+// For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+function lessThan10(items) {
+  let newArray = items.filter((item) => {
+    return item.price < 10;
+  });
+
+  return newArray;
+}
+
+const inputItems2 = [
+  { name: "chair", price: 100 },
+  { name: "pencil", price: 1 },
+  { name: "book", price: 4 },
+];
+const result4 = lessThan10(inputItems2);
+console.log(result4);
+
+// Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
+// For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1: {id: 1, color: "blue", price: 32}, 2: {id: 2, color: "red", price: 12}}.
+
+function converter(items) {
+  let hash = {};
+  let i = 0;
+
+  while (i < items.length) {
+    let key = items[i]["id"];
+    let value = items[i];
+    hash[key] = value;
+    i++;
+  }
+
+  return hash;
+}
+
+const inputItems3 = [
+  { id: 1, color: "blue", price: 32 },
+  { id: 2, color: "red", price: 12 },
+];
+const result5 = converter(inputItems3);
+console.log(result5);
