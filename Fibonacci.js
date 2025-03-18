be717ee9b31d8d339fd41a8cbbe9c39011e533a6
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-03-18 15:44:13 -0400

    Create Fibonacci.js

---
+++ Fibonacci.js
@@ -0,0 +1,20 @@
+function fibonacciGenerator(n) {
+  let output = [];
+
+  if (n === 1) {
+    output.push(0);
+  } else if (n === 2) {
+    output.push(0, 1);
+  } else {
+    output.push(0, 1);
+    for (var i = 2; i < n; i++) {
+      output.push(output[i - 1] + output[i - 2]);
+    }
+  }
+
+  return output;
+}
+
+const n = 3;
+const result = fibonacciGenerator(n);
+console.log(result);
