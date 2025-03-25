3b0b3cbe0314300da08ef3d4af87454932977cf4
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-03-25 16:04:06 -0400

    Create practice199.js

--- practice199.js
+++ practice199.js
@@ -8,9 +8,9 @@ function fibonacciGenerator(n) {
   } else if (n === 2) {
     output = [0, 1];
   } else {
-    output.push(0, 1);
+    output = [0, 1];
     for (let i = 2; i < n; i++) {
-      output.push(output[i - 1] + output[i - 2]);
+      output.push(output[i - 2] + output[i - 1]);
     }
   }
 
@@ -20,3 +20,28 @@ function fibonacciGenerator(n) {
 const inputNum = 10;
 const result = fibonacciGenerator(inputNum);
 console.log(result);
+
+// Fizz Buzz
+
+let output = [];
+let count = 1;
+
+function fizzBuzz(num) {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count);
+  }
+
+  count++;
+}
+
+for (let i = 0; i <= 30; i++) {
+  fizzBuzz();
+}
+
+console.log(output);
