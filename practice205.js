73363dc3dce69fa093d12df2e8f538d21899af0a
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-04-03 08:04:26 -0400

    Added problem to 205.js

--- practice205.js
+++ practice205.js
@@ -49,3 +49,27 @@ const inputString1 = ["a", "b", "c"];
 const inputString2 = ["d", "e", "f", "g"];
 const result2 = stringCombiner(inputString1, inputString2);
 console.log(result2);
+
+// Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+// For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+
+function combinations(string) {
+  let newArray = [];
+  let index1 = 0;
+
+  while (index1 < string.length) {
+    let index2 = 0;
+    while (index2 < string.length) {
+      if (index1 !== index2) {
+        newArray.push(string[index1] + string[index2]);
+      }
+      index2++;
+    }
+    index1++;
+  }
+  return newArray;
+}
+
+const inputString = ["a", "b", "c", "d"];
+const result3 = combinations(inputString);
+console.log(result3);
