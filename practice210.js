5b67c3686e9d093a14962f2ef353f36193ed5f4f
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-04-10 11:42:15 -0400

    Create practice210.js

---
+++ practice210.js
@@ -0,0 +1,27 @@
+// Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
+// For example, {"chair": 75, "book": 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair": {price: 75, color: "red", weight: 10}, "book": {price: 15, color: "black", weight: 1}}.
+
+function combiner(prices, info) {
+  let newHash = {};
+  let i = 0;
+
+  while (i < info.length) {
+    let item = info[i];
+    let name = item.name;
+    let color = item.color;
+    let weight = item.weight;
+    let price = prices[name];
+    newHash[name] = { price: price, color: color, weight: weight };
+    i++;
+  }
+
+  return newHash;
+}
+
+const inputPrices = { chair: 75, book: 15 };
+const inputInfo = [
+  { name: "chair", color: "red", weight: 10 },
+  { name: "book", color: "black", weight: 1 },
+];
+const result = combiner(inputPrices, inputInfo);
+console.log(result);
