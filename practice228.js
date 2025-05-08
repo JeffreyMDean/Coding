5617d304b52ef11052af217b27869ad5b8b69500
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-05-08 14:40:05 -0400

    Added practice problems to 228.js

--- practice228.js
+++ practice228.js
@@ -24,3 +24,46 @@ const inputSpecs = [
 ];
 const result = combiner(inputPrices, inputSpecs);
 console.log(result);
+
+// Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
+// For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
+
+function converter(books) {
+  let hash = {};
+
+  for (let i = 0; i < books.length; i++) {
+    let author = books[i].author;
+    let title = books[i].title;
+    if (hash[author] === undefined) {
+      hash[author] = [];
+    }
+    hash[author].push(title);
+  }
+
+  return hash;
+}
+
+const inputBooks = [
+  { author: "Jeff Smith", title: "Bone" },
+  { author: "George Orwell", title: "1984" },
+  { author: "Jeff Smith", title: "RASL" },
+];
+const result2 = converter(inputBooks);
+console.log(result2);
+
+// Given a hash, create a new hash that has the keys and values switched.
+// For example, {"a": 1, "b": 2, "c": 3} becomes {1: "a", 2: "b", 3: "c"}.
+
+function switcher(hash) {
+  let newHash = {};
+
+  Object.entries(hash).forEach(([key, value]) => {
+    newHash[value] = key;
+  });
+
+  return newHash;
+}
+
+const inputHash = { a: 1, b: 2, c: 3 };
+const result3 = switcher(inputHash);
+console.log(result3);
