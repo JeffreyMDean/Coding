9530e790f2f37080cc76c90e00b44412253d2229
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-06-05 14:01:56 -0400

    Added to practice240.js

--- practice240.js
+++ practice240.js
@@ -26,3 +26,32 @@ function largestProduct(numbers) {
 const inputNumbers = [5, -2, 1, -9, -7, 2, 6];
 const result = largestProduct(inputNumbers);
 console.log(result);
+
+// Use a nested loop to convert an array of number pairs into a single flattened array.
+// For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
+
+function converter2(numberPairs) {
+  let flattenedArray = [];
+  let index1 = 0;
+
+  while (index1 < numberPairs.length) {
+    let numberPair = numberPairs[index1];
+    let index2 = 0;
+    while (index2 < numberPair.length) {
+      let number = numberPair[index2];
+      flattenedArray.push(number);
+      index2++;
+    }
+    index1++;
+  }
+
+  return flattenedArray;
+}
+
+const inputNumberPairs = [
+  [1, 3],
+  [8, 9],
+  [2, 16],
+];
+const result2 = converter2(inputNumberPairs);
+console.log(result2);
