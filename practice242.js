299a05ffd7e426904678a6c5015424318054224d
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2025-06-08 15:36:23 -0400

    Added to practice242.js

--- practice242.js
+++ practice242.js
@@ -17,3 +17,23 @@ function convertToInteger(number) {
 const inputNumber = 7.77;
 const result2 = convertToInteger(inputNumber);
 console.log(result2);
+
+// Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
+// For example, ["do", "or", "do", "not"] becomes {"do": 2, "or": 1, "not": 1}.
+
+function converter(strings) {
+  let hash = {};
+
+  for (let i = 0; i < strings.length; i++) {
+    if (hash[strings[i]] === undefined) {
+      hash[strings[i]] = 0;
+    }
+    hash[strings[i]]++;
+  }
+
+  return hash;
+}
+
+const inputStrings = ["do", "or", "do", "not"];
+const result3 = converter(inputStrings);
+console.log(result3);
